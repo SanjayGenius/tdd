ebdc6626f32d7f69cfa3be728efcba698487b8c4
Sanjay Singh <sanjaysingh@e2infosystems.com>
2019-09-27 14:24:56 +0530

    1. Removed unused code

--- src/main/java/org/java/training/tdd/controller/CarNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.java.training.tdd.controller;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-class CarNotFoundException extends RuntimeException {
-    CarNotFoundException() {}
-}
